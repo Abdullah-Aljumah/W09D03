d9c6aea58f899ddd4d47941946e01a63c4928093
Abdullah-Aljumah <Abdullah.Ali.elq.js@tuwaiq.edu.sa>
2021-12-07 16:36:50 +0300

    get user data from backend

--- w09d03/src/App.js
+++ w09d03/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Register from "./components/Register";
 import Login from "./components/Login";
 import Tasks from "./components/Tasks";
 import Register from "./components/Register";
--- w09d03/src/components/Register/index.js
+++ w09d03/src/components/Register/index.js
@@ -1,11 +1,48 @@
-import React from 'react'
-
+import axios from "axios";
+import React from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
+import Swal from "sweetalert2";
 const Register = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const navigate = useNavigate();
 
-export default Register
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("61a5f3da99ca3c5064ba5c6d");
+  const [users, setUsers] = useState([]);
+
+  const getData = async () => {
+    const items = await axios.get(`${process.env.REACT_APP_BASE_URL}/users`);
+    setUsers(items.data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <h1> Register </h1>
+
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <input type="submit" value="Register" className="btn btn-primary" />
+    </div>
+  );
+};
+
+export default Register;
